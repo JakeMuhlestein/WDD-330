705aabe82db81cc88e105a99e737252a20bd6dfd
JakeMuhlestein <48039633+JakeMuhlestein@users.noreply.github.com>
2021-05-30 15:42:05 -0600

    Update storeinfo.js

--- todo/javascript/storeinfo.js
+++ todo/javascript/storeinfo.js
@@ -1,3 +1,5 @@
+// Had difficulty coming up with this. I referenced other students examples to try to figure it out.
+
 //Save to local storage
 export function storeLS(key, list) {
 	localStorage.setItem(key, JSON.stringify(list));
