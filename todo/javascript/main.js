b59234b80e298e642e6e7577b0449c211f8a0955
JakeMuhlestein <48039633+JakeMuhlestein@users.noreply.github.com>
2021-06-12 08:33:18 -0600

    Update main.js

---
+++ todo/javascript/main.js
@@ -0,0 +1,23 @@
+import ToDos from './Todofunc.js';
+
+const ToDoList = new ToDos('todo');
+const input = document.getElementById('newTask');
+const addNew = document.getElementById('addTask');
+
+
+
+window.addEventListener('load', () => {
+  myToDoList.showToDoList();
+  myToDoList.addTabListeners();
+});
+
+addNew.addEventListener('click', () => {
+  myToDoList.addToDo();
+});
+
+inputField.addEventListener('keyup', function(event) {
+  if(event.keyCode === 13) {
+    event.preventDefault();
+    addNew.click();
+  }
+});
