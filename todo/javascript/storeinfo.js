d9a71b804860ad51464a08d0062b462a08be6c16
JakeMuhlestein <48039633+JakeMuhlestein@users.noreply.github.com>
2021-06-12 15:53:03 -0600

    Update storeinfo.js

--- todo/javascript/storeinfo.js
+++ todo/javascript/storeinfo.js
@@ -5,7 +5,7 @@ export function storeLS(key, list) {
 	localStorage.setItem(key, JSON.stringify(list));
 }
 
-//read from local storage
+//Read from local storage
 export function recallLS(key) {
 	let listArray = JSON.parse(localStorage.getItem(key))
 	return listArray;
