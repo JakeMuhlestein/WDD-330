02b55f1230b8ec32dd64305b195e13d2cbf01127
JakeMuhlestein <48039633+JakeMuhlestein@users.noreply.github.com>
2021-06-14 19:24:18 -0600

    Update main.js

--- todo/javascript/main.js
+++ todo/javascript/main.js
@@ -1,23 +1,46 @@
 import ToDos from './todofunc.js';
+import from './storeinfo.js';
+import * as utility from './helperfunc.js';
 
-const newToDoList = new ToDos('todo');
-const input = document.getElementById('newTask');
-const addNew = document.getElementById('addTask');
+//Event Listeners
+listForm.addEventListener("submit", addTask);
+listForm.addEventListener("submitSuccess"), () => showList(list);
+listForm.addEventListener("submitSuccess", storeLS);
 
 
+//Event listener for clicks
+tasks.addEventListener("click", function(event){
+    const id = parseInt(event.target.value);
+    if(event.target.matches("button")) {
+        deleteItem(id);
+    };
 
-window.addEventListener('load', () => {
-  newToDoList.showToDoList();
-  newToDoList.addTabListeners();
+    if(event.target.matches("input[type = 'checkbox']")) {
+        completedTasks(id);
+
+    };
 });
 
-addNew.addEventListener('click', () => {
-  newToDoList.addToDo();
+// Listners for filter button clicks
+
+//Filters by all tasks
+document.querySelector(`.filter-button`).addEventListener("click", function(event){
+    if(event.target.matches("#allTasks")) {
+        displayTasks(toDoList);
+        
+    };
+    //filters by unfinished tasks
+    if(event.target.matches("#pendingTasks")) {
+        filterNotFinished();
+        displayTasks(unfinishedList);
+
+    };
+    //filter tasks that are compelted
+    if(event.target.matches("#compTasks")) {
+        filterFinished();
+        displayTasks(finishedList);
+    };
 });
 
-inputField.addEventListener('keyup', function(event) {
-  if(event.keyCode === 13) {
-    event.preventDefault();
-    addNew.click();
-  }
-});
+//This is calling the get tasks function that retrieves information from local storage
+getTasks();
